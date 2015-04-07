8bf31d783b10dd4b26e26ae5c0b5611f7ff281f5
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-07 02:00:07 +0000

    testingagain

--- task_01.py
+++ task_01.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Just some docstring"""
-import pprint
-
-CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
-             3: {'name': 'Person Two', 'email': 'email@two.com'}
-             }
-ORDERS = {1: {'customer_id': 2, 'total': 10},
-          3: {'customer_id': 2, 'total': 10},
-          4: {'customer_id': 3, 'total': 15}
-          }
-
 
 def sum_orders(customers, orders):
     new_dict = customers.copy()
     for customerkey, customerinfo in customers.iteritems():
-        customerinfo.update({'orders': 0, 'total':0})
+        customerinfo.update({'orders': 0, 'total': 0})
         for field1, value1 in orders.iteritems():
-           if customerkey == value1['customer_id']:
-               customerinfo['total'] += value1['total']
-               customerinfo['orders'] += 1
+            if customerkey == value1['customer_id']:
+                customerinfo['total'] += value1.get('total')
+                customerinfo['orders'] += 1
     return new_dict
-
-            
-if __name__ == '__main__':
-    pprint.pprint(sum_orders(CUSTOMERS, ORDERS))
