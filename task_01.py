dec8bd75d57e0d1808f3bb453780bdf902039dfa
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-10 00:18:59 +0000

    test

--- task_01.py
+++ task_01.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Just some docstring"""
+import data
+import pprint
+
 
 def sum_orders(customers, orders):
-    new_dict = customers.copy()
-    for customerkey, customerinfo in customers.iteritems():
-        customerinfo.update({'orders': 0, 'total': 0})
-        for field1, value1 in orders.iteritems():
-            if customerkey == value1['customer_id']:
-                customerinfo['total'] += value1.get('total')
-                customerinfo['orders'] += 1
-    return new_dict
+    """Process Customer orders, total and count of orders by customer.
+    Args:
+        customers (dict): Dictionary of Customers name and email.
+        orders (dict): Dictionary of Orders containing total ammounts.
+    Returns:
+        results (dict): Dict. of total ammount of orders, and total order count.
+    Examples:
+        >>> ORDERS = {1: {'customer_id': 2, 'total': 10},
+                  3: {'customer_id': 2, 'total': 10},
+                  4: {'customer_id': 3, 'total': 15}}
+        >>> CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
+                         3: {'name': 'Person Two', 'email': 'email@two.com'}}
+        >>> sum_orders(customers=CUSTOMERS, orders=ORDERS)
+        {2: {'name': 'Person One',
+             'email': 'email@one.com',
+             'orders': 2,
+             'total': 20}
+         3: {'name': 'Person Two',
+             'email': 'email@two.com',
+             'orders': 1,
+             'total': 15}}
+    """
+    results = {}
+    for value in orders.itervalues():
+        customerid = value['customer_id']
+        total = value['total']
+        if customerid in customers.keys():
+            if customerid in results:
+                results[customerid]['total'] += total
+                results[customerid]['orders'] += 1
+            else:
+                results.update({customerid:{'total': total}})
+                results[customerid].update({'orders': 0})
+                results[customerid]['orders'] += 1
+                results[customerid].update(customers[customerid])
+    return results
+
+    #pprint.pprint(results)
+
+
+if __name__ == '__main__':
+    pprint.pprint(sum_orders(data.CUSTOMERS, data.ORDERS))
